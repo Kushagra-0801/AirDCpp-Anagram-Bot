001e92ad8e857e4428b9cbbb0b071383d03848f8
Kushagra Gupta <kushgpt08@gmail.com>
2019-09-14 01:03:04 +0530

    add get_questions generator function

--- helpers.py
+++ helpers.py
@@ -4,11 +4,16 @@ Helper functions for help with the extension.
         Wordlist used can be changed by changing the value of the variable WORD_LIST.
     2 > get_anagram_hub_id(api_url: str, auth_header: dict) -> int:
         Return the seesion_id for anagram game hub.
-    3 > run_as_main: Call make_dict
+    3 > get_questions(api_url: str, auth_header: dict, hub_id: int):
+        Generator function that looks for messages that contain a new anagram word
+        and yield them without spaces and lowercased.
+        Note: It is possible that some messages be skipped from in between.
+    4 > run_as_main: Call make_dict
 """
 
 from collections import namedtuple, Counter
 import pickle
+import re
 import requests
 
 WORD_LIST = "wordlist.txt"
@@ -53,6 +58,28 @@ def get_anagram_hub_id(api_url: str, auth_header: dict) -> int:
     for hub in hubs:
         if hub["identity"]["name"] == "Anagram Game Hub":
             return hub["id"]
+
+
+def get_questions(api_url: str, auth_header: dict, hub_id: int):
+    """
+    Generator function that looks for messages that contain a new anagram word
+    and yield them without spaces and lowercased.
+    Note: It is possible that some messages be skipped from in between.
+    """
+    QUESTION_REGEX = re.compile(r"\*\*\* New Anagram Word is \[ (?P<letters>(\w\s)+)")
+    latest_message_id = 0
+    while True:
+        messages = requests.get(
+            f"http://{api_url}hubs/{hub_id}/messages/5", headers=auth_header
+        ).json()
+        for message in messages:
+            if message["id"] <= latest_message_id:
+                continue
+            latest_message_id = message["id"]
+            match = re.search(QUESTION_REGEX, message["text"])
+            if match:
+                letters = match.group("letters")
+                yield letters.lower().replace(" ", "")
 
 
 def make_dict(word_list: str) -> None:
